f0076491b785f87cd6568e592827e7f1dc4a4e10
Sk Riyaz <riyaz.skk1@gmail.com>
2025-03-19 15:24:25 +0530

    url update applied

---
+++ src/config/email_logo_config.php
@@ -0,0 +1,4 @@
+<?php
+// Configuration file for email logo URL
+define('EMAIL_LOGO_URL', 'https://cdn.emailacademy.com/user/fecdcd5176d5ee6a27e1962040645abfa28cce551d682738efd2fc3e158c65e3/majisticlogo2025_03_18_22_18_20.png');
+?>
